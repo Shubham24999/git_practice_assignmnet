f9d80274a4080843fffc5e501ac083f49a9a1548
unknown <shubhamgupta240999@gmail.com>
2022-10-20 12:17:44 +0530

    palindrome no

---
+++ palindrome_no.js
@@ -0,0 +1,11 @@
+let str="asdfdsa";
+let store=str;
+let rev_str;
+for(let i=str.length-1;i>=0;i++){
+    rev_str=str[i];
+}
+if(store=rev_str){
+  console.log("yes:);
+}else{
+   console.log("no");
+};
